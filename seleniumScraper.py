3dee1ebe1f11edf494c432e1a0da5a1656c70c37
даня <62217711+sl1mslav@users.noreply.github.com>
2021-10-04 00:28:24 +0500

    Update seleniumScraper.py

--- seleniumScraper.py
+++ seleniumScraper.py
@@ -10,6 +10,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('window-size=1920x1080')
 
 
 TOKEN = "2003097102:AAHcvmuMAIPRNzlKRfVFMsjegOQIOYUslfw"
